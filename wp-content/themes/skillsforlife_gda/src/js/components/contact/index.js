a92d6b229e7283071168b0fb63378ee528f567f2
Reva Athallah <reva@gaiada.com>
2026-01-29 16:44:55 +0800

    fix: better response output and validation for contact form

--- wp-content/themes/skillsforlife_gda/src/js/components/contact/index.js
+++ wp-content/themes/skillsforlife_gda/src/js/components/contact/index.js
@@ -11,6 +11,16 @@ const initContact = (component) => {
     window.addEventListener('resize', overlayChange)
     overlayChange()
 
+    const formInit = () => {
+        const formEl = component.querySelector('form.wpcf7-form')
+        const responseWrapper = component.querySelector('.wpcf7-response-output')
+        const copyResponseWrapper = responseWrapper.cloneNode(true)
+        copyResponseWrapper.classList.add('clone')
+        formEl.prepend(copyResponseWrapper)
+        responseWrapper.remove()
+    }
+    formInit()
+
 }
 
 export {initContact}
