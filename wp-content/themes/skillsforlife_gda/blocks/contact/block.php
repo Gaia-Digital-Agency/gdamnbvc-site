ffe7ba373594f24db9e9f3642c42188d7dda0045
Reva Athallah <reva@gaiada.com>
2026-01-29 16:39:48 +0800

    fix: match the design with large breakpoints

--- wp-content/themes/skillsforlife_gda/blocks/contact/block.php
+++ wp-content/themes/skillsforlife_gda/blocks/contact/block.php
@@ -29,20 +29,20 @@ $form = get_field('form');
 
 ?>
 <section class="<?= esc_attr($classes) ?> relative" id="<?= esc_attr($id) ?>">
-    <div class="absolute bg-[#E9E9E9] right-0 top-0 bottom-0 overlay hidden md:block"></div>
-    <div class="container md:pb-[200px]">
-        <p class="text-h1 font-extend font-medium md:absolute text-center md:text-left z-10 py-[35px]"><?= $textTitle ?></p>
+    <div class="lg:absolute bg-[#E9E9E9] right-0 top-0 bottom-0 overlay hidden lg:block"></div>
+    <div class="container xl:pb-[220px] lg:pb-[160px]">
+        <?= $textTitle ?>
     </div>
     <div class="relative container">
-        <div class="grid grid-cols-12 gap-y-[60px]">
+        <div class="grid grid-cols-12 gap-y-[60px] lg:gap-x-[50px]">
     
-            <div class="md:col-span-6 col-span-12">
+            <div class="lg:col-span-6 col-span-12">
                 <div class="inner contact-form">
                     <!-- form -->
-                    <!-- <div class="box mb-5 px-[30px] md:w-[600px] h-[60px] border border-theme-black flex items-center text-[rgba(23,23,25,.3)]">First Name</div>
-                    <div class="box mb-5 px-[30px] md:w-[600px] h-[60px] border border-theme-black flex items-center text-[rgba(23,23,25,.3)]">Last Name</div>
-                    <div class="box mb-[50px] px-[30px] py-3 md:w-[600px] h-[300px] border border-theme-black flex text-[rgba(23,23,25,.3)]">Message</div>
-                    <div class="button md:py-[38px] md:w-[600px] py-4 font-extend font-medium text-cta bg-theme-black text-white text-center">
+                    <!-- <div class="box mb-5 px-[30px] lg:w-[600px] h-[60px] border border-theme-black flex items-center text-[rgba(23,23,25,.3)]">First Name</div>
+                    <div class="box mb-5 px-[30px] lg:w-[600px] h-[60px] border border-theme-black flex items-center text-[rgba(23,23,25,.3)]">Last Name</div>
+                    <div class="box mb-[50px] px-[30px] py-3 lg:w-[600px] h-[300px] border border-theme-black flex text-[rgba(23,23,25,.3)]">Message</div>
+                    <div class="button lg:py-[38px] lg:w-[600px] py-4 font-extend font-medium text-cta bg-theme-black text-white text-center">
                         Send Message
                     </div> -->
                     <?php 
@@ -53,11 +53,11 @@ $form = get_field('form');
                 </div>
             </div>
     
-            <div class="md:col-span-6 col-span-12 bg-[#E9E9E9] blocks-wrapper pb-[30px] md:pb-0 -container-mobile">
+            <div class="lg:col-span-6 col-span-12 bg-[#E9E9E9] blocks-wrapper pb-[30px] lg:pb-0 -container-mobile">
                 <InnerBlocks template="<?= esc_attr(wp_json_encode($template)) ?>" />
             </div>
             <div class="col-span-12">
-                <div class="spacer md:pt-[200px] pt-[60px]"></div>
+                <div class="spacer lg:pt-[200px] pt-[60px]"></div>
             </div>
         </div>
     
